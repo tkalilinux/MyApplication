d2a6d83c9f4d3c9d07c0f89a9251d16881190f6f
Tên của bạn <email@example.com>
2025-05-19 13:32:09 +0700

    Cập nhật chức năng đăng nhập

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,7 +7,7 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    //    Test update
+    //    Test update 23456
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
